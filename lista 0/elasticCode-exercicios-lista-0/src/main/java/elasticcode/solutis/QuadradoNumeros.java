f8a771e91057b8d528577492c3a8cc7d0a062935
raulreis123 <raulgomesreis@gmail.com>
2025-04-30 15:14:13 -0300

    Limpando branch

--- lista 0/elasticCode-exercicios-lista-0/src/main/java/elasticcode/solutis/QuadradoNumeros.java
+++
@@ -1,9 +0,0 @@
-package elasticcode.solutis;
-
-public class QuadradoNumeros {
-    public static void main(String[] args){
-        for(int i = 1; i <= 10; i++){
-            System.out.printf("\nQuadrado de %d: %d%n", i, i * i);
-        }
-    }
-}
